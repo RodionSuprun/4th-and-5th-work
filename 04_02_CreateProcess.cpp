1c3775a5472a2c14228e86a1163b07fa03a4efbf
Rodion <rsuprun97@gmail.com>
2017-09-15 23:20:35 +0600

    second commit in second branch

--- 04_02_CreateProcess.cpp
+++ 04_02_CreateProcess.cpp
@@ -13,7 +13,7 @@ si.cb = sizeof(STARTUPINFO);
 if(!CreateProcess(L"C:\\createProcess.exe", NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp))   
 {     
 _cputs("The new process is not created.\n");    
-_cputs("Check a name of the process.\n");     
+_cputs("Check a IGNAT of the process.\n");     
 _cputs("Press any key to finish.\n");     
 _getch(); 
 return 0;   
